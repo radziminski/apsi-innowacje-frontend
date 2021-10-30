a663e2222b7eb64343da1132a543f0a333cdea70
Jan Radziminski <radziminski.j@gmail.com>
2021-10-30 14:45:17 +0200

    feat: project setup

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
+    quotes: ['error', 'single'],
+    semi: [2, 'always'],
+    'max-len': [2, { code: 120, tabWidth: 2, ignoreUrls: true, ignoreComments: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
---
+++ babel.config.js
@@ -0,0 +1,42 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: ['@babel/preset-react', '@babel/preset-typescript'],
+    env: {
+      production: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              useBuiltIns: 'entry',
+              corejs: 3,
+              targets: {
+                node: 'current',
+                browsers: ['>0.2%', 'not dead', 'not op_mini all'],
+              },
+            },
+          ],
+        ],
+      },
+      development: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              useBuiltIns: 'entry',
+              corejs: 3,
+              targets: {
+                node: 'current',
+                browsers: ['last 1 version'],
+              },
+            },
+          ],
+        ],
+      },
+      testing: {
+        presets: ['@babel/env'],
+      },
+    },
+  };
+};
