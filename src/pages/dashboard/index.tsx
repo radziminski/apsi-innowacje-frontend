bffd7a48c0f8425c1c9250c626bec6232cc69f5b
MBelniak <rubik40s@wp.pl>
2021-11-01 20:53:13 +0100

    Fix tabs in dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,15 +6,15 @@ import DashboardLayout from './layout';
 import CreateIdeaPage from './create-idea-page/CreateIdeaPage';
 
 export const DashboardRoutes: React.FC = () => {
-    return (
-        <DashboardLayout>
-            <Switch>
-                <Route path={getExamplePagePath()} component={ExamplePage} />
-                <Route path={getCreateIdeaPath()} component={CreateIdeaPage} />
-                {/* Rest of dashboard routes here */}
-            </Switch>
-        </DashboardLayout>
-    );
+  return (
+    <DashboardLayout>
+      <Switch>
+        <Route path={getExamplePagePath()} component={ExamplePage} />
+        <Route path={getCreateIdeaPath()} component={CreateIdeaPage} />
+        {/* Rest of dashboard routes here */}
+      </Switch>
+    </DashboardLayout>
+  );
 };
 
 export default DashboardRoutes;
