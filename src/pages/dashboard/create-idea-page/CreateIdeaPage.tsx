a9296d62fa8969418e0fe3df64e162b74e9aa20f
MBelniak <rubik40s@wp.pl>
2021-11-01 20:20:43 +0100

    Add prettier, add create idea page

---
+++ src/pages/dashboard/create-idea-page/CreateIdeaPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CreateIdeaPage = (): JSX.Element => {
+
+    return <div>
+        Idea page
+    </div>;
+};
+
+export default CreateIdeaPage;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { getExamplePagePath } from '~/constants/paths';
+import { getCreateIdeaPath, getExamplePagePath } from '~/constants/paths';
 import ExamplePage from './example-page';
 import DashboardLayout from './layout';
+import CreateIdeaPage from './create-idea-page/CreateIdeaPage';
 
 export const DashboardRoutes: React.FC = () => {
-  return (
-    <DashboardLayout>
-      <Switch>
-        <Route path={getExamplePagePath()} component={ExamplePage} />
-        {/* Rest of dashboard routes here */}
-      </Switch>
-    </DashboardLayout>
-  );
+    return (
+        <DashboardLayout>
+            <Switch>
+                <Route path={getExamplePagePath()} component={ExamplePage} />
+                <Route path={getCreateIdeaPath()} component={CreateIdeaPage} />
+                {/* Rest of dashboard routes here */}
+            </Switch>
+        </DashboardLayout>
+    );
 };
 
 export default DashboardRoutes;
