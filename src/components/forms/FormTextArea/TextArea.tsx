33133988f1256ebf04e17c8f22832fb07127587e
MBelniak <rubik40s@wp.pl>
2021-12-15 18:36:38 +0100

    Fix form

--- src/components/forms/FormTextArea/TextArea.tsx
+++ src/components/forms/FormTextArea/TextArea.tsx
@@ -2,30 +2,33 @@ import styled from 'styled-components';
 import { FormComponentProps } from '~/components/forms';
 import React from 'react';
 
-const TextInputBase = (props: FormComponentProps) => {
+const TextAreaBase = (props: FormComponentProps) => {
   const { className, register, customClassName, ...rest } = props;
 
   return (
-    <input
+    <textarea
       {...(register ? register : {})}
-      type={props.type || 'text'}
-      className={className + (customClassName ? ` ${customClassName}` : '')}
+      maxLength={1000}
       {...rest}
+      className={className + (customClassName ? ` ${customClassName}` : '')}
     />
   );
 };
 
-export const TextInput = styled(TextInputBase)`
+export const TextArea = styled(TextAreaBase)`
+  border: 1px solid
+    ${props => (props.errors && props.errors[props.id] ? props.theme.colors.error : props.theme.colors.primary)}5A;
+  border-radius: ${({ theme }) => theme.borderRadiuses.normal};
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 15px;
+  resize: none;
   box-shadow: none;
   transition: box-shadow 0.15s ease-in-out;
   width: 100%;
   ::placeholder {
     color: ${({ theme }) => theme.colors.lightGray};
   }
-  border: 1px solid ${({ id, errors, theme }) => (errors && errors[id] ? theme.colors.error : theme.colors.primary)}5A;
-  border-radius: 999px;
-  background-color: ${({ theme }) => theme.colors.white};
-  padding: 0.8rem ${({ theme }) => theme.spacing.s};
+
   &:hover {
     box-shadow: 0 0 0.15rem
       ${({ id, errors, theme }) => (errors && errors[id] ? theme.colors.error : theme.colors.primary)}AF;
--- src/components/forms/FormTextArea/index.tsx
+++ src/components/forms/FormTextArea/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FormComponentProps } from '~/components/forms';
 import { FlexBox } from '~/components/Box';
+import { TextArea } from '~/components/forms/FormTextArea/TextArea';
 
 const FormTextAreaBase = (props: FormComponentProps) => {
   const { id, className, ...rest } = props;
@@ -15,13 +16,7 @@ const FormTextAreaBase = (props: FormComponentProps) => {
   return (
     <MemoizeFormComponent {...methods}>
       <FlexBox className={className}>
-        <textarea
-          {...methods.register(id)}
-          maxLength={1000}
-          className={'form-textarea' + (errors[id] ? '--error' : '')}
-          id={id}
-          {...rest}
-        />
+        <TextArea register={methods.register(id)} errors={errors} id={id} {...rest} />
         {errors[id] && <p>{errors[id].message}</p>}
       </FlexBox>
     </MemoizeFormComponent>
@@ -31,38 +26,6 @@ const FormTextAreaBase = (props: FormComponentProps) => {
 export const FormTextArea = styled(FormTextAreaBase)`
   flex-direction: column;
   width: 100%;
-
-  .form-textarea,
-  .form-textarea--error {
-    border: 1px solid ${({ theme }) => theme.colors.primary}5A;
-    border-radius: ${({ theme }) => theme.borderRadiuses.normal};
-    background-color: ${({ theme }) => theme.colors.white};
-    padding: 15px;
-    resize: none;
-    box-shadow: none;
-    transition: box-shadow 0.15s ease-in-out;
-    width: 100%;
-    ::placeholder {
-      color: ${({ theme }) => theme.colors.lightGray};
-    }
-  }
-
-  .form-textarea {
-    &:hover {
-      box-shadow: 0 0 0.15rem ${({ theme }) => theme.colors.primary}AF;
-    }
-    &--error:hover {
-      box-shadow: 0 0 0.15rem ${({ theme }) => theme.colors.error}AF;
-    }
-
-    &:focus {
-      box-shadow: 0 0 0.25rem ${({ theme }) => theme.colors.primary}AF;
-    }
-
-    &--error:focus {
-      box-shadow: 0 0 0.25rem ${({ theme }) => theme.colors.error}AF;
-    }
-  }
   p {
     margin-top: 5px;
   }
