c53e8cd06a7c482acbd27977eff3ef12024819e8
Jan Radziminski <radziminski.j@gmail.com>
2022-02-02 19:59:01 +0100

    feat: added UI for voting

--- src/pages/dashboard/voting/index.tsx
+++ src/pages/dashboard/voting/index.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdOutlineRateReview } from 'react-icons/md';
 import DashboardContent from '~/components/DashboardContent/DashboardContent';
 import 'suneditor/dist/css/suneditor.min.css';
@@ -8,13 +7,14 @@ import { useSelector } from '~/store/hooks';
 import { getIdeas, getSubjects, getVotesForSubject } from '~/store/slices/CreateIdeasSlice';
 import AsyncContentContainer from '~/components/AsyncContentContainer';
 import { useDispatch } from 'react-redux';
-import { SubjectButton } from './parts';
-import { Heading6 } from '~/components/Text';
-import { COLORS } from '~/styles/variables';
+import { SubjectButton, RatingButton, SendButton } from './parts';
+import Text, { Heading4, Heading6 } from '~/components/Text';
 import IdeaCard from '~/components/IdeaCard';
 
 export const VotingPage: React.FC = () => {
   const [selectedSubject, setSelectedSubject] = useState<number>();
+  const [selectedVotes, setSelectedVotes] = useState<Record<number, number>>({});
+
   const {
     subjects,
     isLoadingSubjects,
@@ -50,6 +50,23 @@ export const VotingPage: React.FC = () => {
     }
   }, [filteredSubjects]);
 
+  const currVotes = selectedSubject && subjectVotes[selectedSubject];
+
+  const setVote = (ideaId: number, voteValue: number) => {
+    setSelectedVotes(prev => {
+      if (Object.values(prev).includes(ideaId)) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const entry = Object.entries(prev).find(([_, value]) => value === ideaId);
+        if (entry) delete prev[entry[0]];
+      }
+
+      return {
+        ...prev,
+        [voteValue]: ideaId
+      };
+    });
+  };
+
   return (
     <DashboardContent title="Głosowanie na pomysły" icon={<MdOutlineRateReview size={28} />}>
       <FlexBox
@@ -62,6 +79,7 @@ export const VotingPage: React.FC = () => {
             Wybierz temat pomysłów do głosowania:
           </Heading6>
           <Box height="1rem" flexShrink={0} />
+
           <FlexBox flexWrap="wrap" gap="1.5rem">
             {filteredSubjects?.length ? (
               filteredSubjects.map(subject => (
@@ -81,15 +99,49 @@ export const VotingPage: React.FC = () => {
               </Box>
             )}
           </FlexBox>
+          {!!currVotes && Object.keys(selectedVotes).length < currVotes && (
+            <Box marginTop="2rem">
+              <Heading4 fontWeight={600}>Pozostałe głosy: </Heading4>
+              <FlexBox gap="1rem" marginTop="0.5rem">
+                {Array.from(Array(currVotes).keys())
+                  .filter(key => !Object.keys(selectedVotes).includes('' + (key + 1)))
+                  .map(key => (
+                    <RatingButton key={key + 1}>
+                      <Text fontSize="1.15rem">{key + 1}</Text>
+                    </RatingButton>
+                  ))}
+              </FlexBox>
+            </Box>
+          )}
+
           {!!filteredSubjects?.length && (
             <Box marginTop="1.5rem">
               {selectedSubject ? (
                 <AsyncContentContainer isLoading={isLoading || isLoadingVotes} isError={isError || isVotesError}>
                   {filteredIdeas?.map(idea => (
-                    <Box>
+                    <Box key={idea.id}>
                       <IdeaCard idea={idea} votingMode />
+                      <Box paddingLeft="1.5rem" marginBottom="3rem">
+                        <Heading4>Oceń pomysł: </Heading4>
+                        <FlexBox gap="1rem" marginTop="0.5rem">
+                          {Array.from(Array(currVotes).keys()).map(key => (
+                            <RatingButton
+                              key={key}
+                              onClick={() => idea.id && setVote(idea.id, key + 1)}
+                              isSelected={selectedVotes[key + 1] === idea.id}>
+                              <Text fontSize="1.15rem">{key + 1}</Text>
+                            </RatingButton>
+                          ))}
+                        </FlexBox>
+                      </Box>
                     </Box>
                   ))}
+
+                  {
+                    <Box opacity={Object.keys(selectedVotes).length ? 1 : 0} transition="all 0.2s">
+                      <SendButton>Wyślij oceny</SendButton>
+                    </Box>
+                  }
                 </AsyncContentContainer>
               ) : (
                 <Box marginY="25vh" textAlign="center" flexGrow={1}>
--- src/pages/dashboard/voting/parts.tsx
+++ src/pages/dashboard/voting/parts.tsx
@@ -15,3 +15,50 @@ export const SubjectButton = styled.button<{ isSelected: boolean }>`
     color: ${({ theme, isSelected }) => (isSelected ? theme.colors.white : theme.colors.primaryHover)};
   }
 `;
+
+export const RatingButton = styled.button<{ isSelected?: boolean }>`
+  width: 2.5rem;
+  height: 2.5rem;
+  font-size: 1.15rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 2px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme, isSelected }) => (isSelected ? theme.colors.white : theme.colors.primary)};
+  font-size: 1rem;
+  border-radius: ${({ theme }) => theme.borderRadiuses.small};
+  background: ${({ theme, isSelected }) => (isSelected ? theme.colors.primary : 'transparent')};
+  transition: all 0.2s;
+
+  &:hover {
+    background: ${({ theme, isSelected }) => (isSelected ? theme.colors.primaryHover : theme.colors.primary)};
+    border: 2px solid ${({ theme, isSelected }) => (isSelected ? theme.colors.primaryHover : theme.colors.primary)};
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const SendButton = styled.button<{ disabled?: boolean }>`
+  padding: 1rem 2rem;
+  font-size: 1.15rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: fixed;
+  right: 4rem;
+  bottom: 4rem;
+  z-index: ${({ theme }) => theme.zIndex.stickedFront};
+
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: ${({ theme }) => theme.borderRadiuses.small};
+  background: ${({ theme }) => theme.colors.primary};
+  transition: all 0.2s;
+
+  box-shadow: 0 0.4rem 0.7rem rgba(0, 0, 0, 0.18);
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primaryHover};
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
